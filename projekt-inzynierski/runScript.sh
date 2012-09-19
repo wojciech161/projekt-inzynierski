e39edf7f0d121604e3905ab7b97928adaaf4c0e4
Marcin Wojciechowski <wojciech161@gmail.com>
2012-09-19 23:56:09 +0200

    Pierwszy commit, ustawiony maven

---
+++ projekt-inzynierski/runScript.sh
@@ -0,0 +1,1 @@
+java -cp target/projekt-inzynierski-1.0-SNAPSHOT.jar pl.mwojciec.app.App
---
+++ projekt-inzynierski/src/main/java/pl/mwojciec/app/App.java
@@ -0,0 +1,9 @@
+package pl.mwojciec.app;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Projekt Inżynierski" );
+    }
+}
